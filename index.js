7169880bf7f1f2b828eb5212a0089bef1b7039a6
Roman Yusupov <romyusupov2015@gmail.com>
2018-12-03 12:42:10 +0500

    change port configuration to deploy to Heroku

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const port = process.env.PORT || 3000;
+
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const laptopData = JSON.parse(json);
 
@@ -38,8 +40,8 @@ const server = http.createServer((req, res) => {
     }
 });
 
-server.listen(1337, '127.0.0.1', () => {
-    console.log('Listening for requests now');
+server.listen(port, () => {
+    console.log(`Server running at port: ${port}`);
 });
 
 function replaceTemplate(originalHtml, laptop) {
